768c85bc030426edc9cd82aa16f65f786788a9c0
PC <PC@DESKTOP-PKQGK0I>
2024-12-06 00:34:54 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_MEJG/MVVM_MEJG/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MVVM_MEJG.Vista;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM_MEJG
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Pagina1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MVVM_MEJG/MVVM_MEJG/Vista/Pagina1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MVVM_MEJG.ModeloVista;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM_MEJG.Vista
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina1 : ContentPage
+    {
+        public Pagina1()
+        {
+            InitializeComponent();
+            BindingContext = new VMPagina1(Navigation);
+        }
+    }
+}
